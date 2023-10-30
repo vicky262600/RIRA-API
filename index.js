7c5ab0296b75607c92cfb87cf709a1305d3ae28f
vicky262600 <vickyjoshi2604@gmail.com>
2023-10-30 12:24:01 -0400

    database connected

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    ).then(()=>console.log("db connection successful"))
+    .catch((err)=>{console.log(err);
+});
+
+app.listen(5000, () =>{
+    console.log("backend server is ready")
+})

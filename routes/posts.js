a5bcc863b018063bd34dabedf18bbee611da4239
vicky262600 <vickyjoshi2604@gmail.com>
2023-11-18 14:30:40 -0500

    API done

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // create a post
 router.post("/", async(req, res)=>{
@@ -14,7 +15,7 @@ router.post("/", async(req, res)=>{
 
 // update a post
 router.put("/:id", async(req,res)=>{
-    const post = await Post.findById(req.param.id);
+    const post = await Post.findById(req.params.id);
     try{
         if(post.userId == req.body.userId){
             await post.updateOne({$set:req.body});
@@ -24,11 +25,65 @@ router.put("/:id", async(req,res)=>{
         }
     }catch(err){
         res.status(500).json(err);
+    } 
+});
+
+// delete a post
+router.delete("/:id", async(req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId == req.body.userId){
+            await post.deleteOne();
+            res.status(200).json("the post has been deleted")
+        }else{
+            res.status(402).json("you can only delete your post ")
+        }
+    }catch(err){
+        res.status(500).json(err);
     }
 })
-// delete a post
-// like a post
+
+// like/ dislike a post
+router.put("/:id/like", async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.like.includes(req.body.userId)){
+            await post.updateOne({ $push:{like: req.body.userId}});
+            res.status(200).json("you have liked the post")
+        }else{
+            await post.updateOne({$pull:{like: req.body.userId}});
+            res.status(200).json("you have disliked the post")
+        }
+    }catch(err){
+        res.status(500).json(err);
+
+    }
+});
 // get a post
-// get all post
+router.get("/:id", async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+// get timeline post
+router.get("/timeline/all", async(req, res)=>{
+    // let postArray = [];
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id});
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId)=>{
+               return Post.find({ userId: friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    }catch(err){
+        res.status(500).json(err);  
+    }
+})
 
 module.exports = router

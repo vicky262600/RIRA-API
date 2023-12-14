8f35e4c0f93554ce9c1aa6fbb32869ea8a8594fd
vicky262600 <vickyjoshi2604@gmail.com>
2023-12-14 10:47:37 -0500

    users done

--- routes/posts.js
+++ routes/posts.js
@@ -95,7 +95,7 @@ router.get("/profile/:username", async (req, res)=>{
         const posts = await Post.find({userId: user._id});
         res.status(200).json(posts);
     }catch(err){
-        res.status(500).json(err);  
+        res.status(500).json(err);
     }
 })
 
